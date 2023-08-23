66374fdd5c315283ee23bfaa26f804f605e2b192
cycsd <cycsd9211@gmail.com>
2023-08-23 14:42:40 +0800

    change name

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
-mod tests{
+mod tests {
     #[test]
-    fn it_works(){
-        assert_eq!(1,1);
+    fn how_to_get_mut_in_vec() {
+        let mut v = [1, 2, 3, 4, 5];
+        let target = v.iter_mut().find(|n| **n == 2);
+        target.map(|t| *t = 6);
+        assert_eq!(v, [1,6,3,4,5]);
     }
-
 }
